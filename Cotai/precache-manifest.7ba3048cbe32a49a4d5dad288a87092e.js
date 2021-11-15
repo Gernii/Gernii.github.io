803c6422d88eec32b81dfe939effbba7b5e198de
litahung <anhducngole2001@gmail.com>
2021-11-15 10:50:25 +0700

    a

---
+++ Cotai/precache-manifest.7ba3048cbe32a49a4d5dad288a87092e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a40115973b5dabc9487b3ce778b99742",
+    "url": "Cotai/index.html"
+  },
+  {
+    "revision": "c1952b2439e82ad97e96",
+    "url": "Cotai/static/css/main.51dba36e.css"
+  },
+  {
+    "revision": "c1952b2439e82ad97e96",
+    "url": "Cotai/static/js/main.0e14b6d8.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "Cotai/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "Cotai/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "Cotai/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "Cotai/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- Cotai/service-worker.js
+++ Cotai/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.890071d6cd0b26d283ca5b67a6856211.js"
+  "Cotai/precache-manifest.7ba3048cbe32a49a4d5dad288a87092e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("Cotai/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
