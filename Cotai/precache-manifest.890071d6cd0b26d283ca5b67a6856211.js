5d0ae3eaa8e9aef640ab30653354cf17980c5fc7
litahung <anhducngole2001@gmail.com>
2021-11-15 10:46:25 +0700

    a

--- Cotai/precache-manifest.890071d6cd0b26d283ca5b67a6856211.js
+++ Cotai/precache-manifest.890071d6cd0b26d283ca5b67a6856211.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "95b5a9c0775a461d5956d116f88c7364",
-    "url": "/index.html"
+    "url": "/Cotai/index.html"
   },
   {
     "revision": "b25b5ec1162a280cfa67",
-    "url": "/static/css/main.53bbb385.css"
+    "url": "/Cotai/static/css/main.53bbb385.css"
   },
   {
     "revision": "b25b5ec1162a280cfa67",
-    "url": "/static/js/main.dab77e7b.js"
+    "url": "/Cotai/static/js/main.dab77e7b.js"
   },
   {
     "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
-    "url": "/static/media/slick.b7c9e1e4.woff"
+    "url": "/Cotai/static/media/slick.b7c9e1e4.woff"
   },
   {
     "revision": "ced611daf7709cc778da928fec876475",
-    "url": "/static/media/slick.ced611da.eot"
+    "url": "/Cotai/static/media/slick.ced611da.eot"
   },
   {
     "revision": "d41f55a78e6f49a5512878df1737e58a",
-    "url": "/static/media/slick.d41f55a7.ttf"
+    "url": "/Cotai/static/media/slick.d41f55a7.ttf"
   },
   {
     "revision": "f97e3bbf73254b0112091d0192f17aec",
-    "url": "/static/media/slick.f97e3bbf.svg"
+    "url": "/Cotai/static/media/slick.f97e3bbf.svg"
   }
 ]);
